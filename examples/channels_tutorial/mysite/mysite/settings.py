9e6437f24957480f35debe39a40515206c858f95
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-21 15:43:09 +0200

    Channels tutorial to end of part 4.

--- examples/channels_tutorial/mysite/mysite/settings.py
+++ examples/channels_tutorial/mysite/mysite/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        },
     }
 }
 
